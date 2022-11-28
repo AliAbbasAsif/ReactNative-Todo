a517733cf3d1771636a372f6feae7b1169cc8da5
AliAbbasAsif <aliabbasasif89@gmail.com>
2022-11-28 21:38:22 +0500

    updated

---
+++ Screens/Login2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Image,  View} from 'react-native';
+
+function Login2() {
+  return (
+      
+      <View>
+      <View style={{backgroundColor:'blue',height:'70%'}}>
+      <Text>Facebook</Text>
+
+      </View>
+      </View>
+  );
+}
+
+export default Login2;
