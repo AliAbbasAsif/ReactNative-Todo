d067eb348f12f9a9805b44679e6f8f84ced21a66
AliAbbasAsif <aliabbasasif89@gmail.com>
2022-11-28 23:12:17 +0500

    pushed

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 import Login2 from './Screens/Login2';
+import Login3 from './Screens/Login3';
+import Login4 from './Screens/Login4';
+import Login5 from './Screens/Login5';
 import Netflixlogin from './Screens/Netflixlogin';
 import Todo from './Screens/Todo';
 
@@ -8,9 +11,12 @@ function App() {
 
   return (
     <View>
-     <Todo/>
+     {/* <Todo/> */}
     {/* <Netflixlogin/> */}
     {/* <Login2/> */}
+    {/* <Login3/> */}
+    {/* <Login4/> */}
+    <Login5/>
     </View>
   );
 }
