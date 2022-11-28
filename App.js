990cd4438bcfedfeadb57614e1f5355e896743d0
AliAbbasAsif <aliabbasasif89@gmail.com>
2022-11-28 21:37:36 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
+import Login2 from './Screens/Login2';
+import Netflixlogin from './Screens/Netflixlogin';
+import Todo from './Screens/Todo';
+
+function App() {
+
+  return (
+    <View>
+     <Todo/>
+    {/* <Netflixlogin/> */}
+    {/* <Login2/> */}
+    </View>
+  );
+}
+
+export default App;
